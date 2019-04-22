f4f0d10ae29ca6c0c9b19771f46566ba3b88929e
xiunaonao <onlymyalways@163.com>
2019-04-23 00:17:24 +0800

    banner

--- banner.js
+++ banner.js
@@ -7,13 +7,14 @@ let socket={
 	},
 	join(){
 		this.io.on('banner',(msg)=>{
-			console.log('大屏幕已加入')
+			console.log('大屏幕已加入'+this.io.id)
 			this.id=this.io.id
 			//this.io.emit()
 		})
 
 	},
 	post(name){
+		console.log('大屏幕'+this.io.id+'回复')
 		this.io.emit('user',name)
 	},
 	
